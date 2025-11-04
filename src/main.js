e6137b84f08138ce0a1907b3c7a5a85c0e628269
KOUSAKU <kousaku@icloud.com>
2025-11-04 15:30:28 +0900

    チェックボックス追加 (#1)

--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
 'use strict'
 
-
-// 入力内容を配列へ渡す
-const food = [];
-function addOption() {
-  const text = document.getElementById("inputText").value;
-  if (!text) return;
-  food.push(text);
-  document.getElementById("stockresult").textContent=food;
-}
-
-// スープと具材を決める仕組み
+// スープを決める
 const soup = ["カレー", "キムチ", "ごま豆乳", "みそ", "ちゃんこ風"];
 
 document.getElementById("soupdecide").addEventListener ("click", function() {
@@ -18,22 +8,61 @@ document.getElementById("soupdecide").addEventListener ("click", function() {
   document.getElementById("soupResult").textContent=soup[index];
 });
 
+// 入力内容を配列へ渡す
+const ourstock = [];
+const candidates = [];
+
+
+document.getElementById("addfood").addEventListener ("click", function () {
+  const text = document.getElementById("inputText").value;
+  if (!text) return;
+  ourstock.push(text);
+  createList();
+  document.getElementById("inputText").value = "";
+  // document.getElementById("stockresult").textContent=food;
+});
+
+ 
+
+// 入力内容からリスト作成し、チェックボックス、indexをふる
+
+function createList () {
+    const list = document.getElementById("candidateList")
+    list.innerHTML = "";
+    ourstock.forEach (function (item, index) {
+      const foodlist = document.createElement("li");
+
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.dataset.index = index;
+
+      const stockfood = document.createTextNode(item)
+      
+      foodlist.appendChild(checkbox);
+      foodlist.appendChild(stockfood);
+      list.appendChild(foodlist);
+    });
+  }
+
+
+  
+
+// 具材を決める
 document.getElementById("fooddecide").addEventListener ("click", function() {
-  if (food.length === 0){
+  // チェックがついたものを候補の配列に入れる
+
+  const candidates = [];
+  const checkboxes = document.querySelectorAll("#candidateList input[type='checkbox']");
+  checkboxes.forEach ((box) => {
+    if (box.checked) {
+      const index =box.dataset.index;
+      candidates.push(ourstock[index]);
+    }
+  });
+  if (candidates.length === 0){
     document.getElementById("foodResult").textContent="冷蔵庫に何もありません";
     return;
   }
-  const index =Math.floor(Math.random() * food.length) ;
-  document.getElementById("foodResult").textContent=food[index];
+  const i =Math.floor(Math.random() * candidates.length) ;
+  document.getElementById("foodResult").textContent=candidates[i];
 });
-
-// 一覧から選択したものだけを配列に渡したい
-// 理解できず保留にしているのは具材の個数もランダムに決まる仕組みのうち、シャッフルのところ。 Fisher–Yates shuffleを理解したら、実装できそう
-// 冷蔵庫にあるもの一覧が更新、記録、削除できる仕組み
-// 冷蔵庫にあるもの一覧から具材候補を選べる仕組み
-// 材料などにキーを割り振るなどして、レシピや揃うと作れるレシピが提案される仕組み
-// レシピには満足度、作るめんどくささ、メモなど残せるシステム
-// しめも入れる
-// 全部決めるとレシピ名が出てくるシステム
-
-// 削除機能追加
